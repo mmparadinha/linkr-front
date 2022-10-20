765e42994c5c28a249e9de238e02ec707c848577
root <root@DESKTOP-QJ6ICDP.localdomain>
2022-10-20 00:25:19 -0300

    getPost query correction

--- src/repository/postsRepository.js
+++ src/repository/postsRepository.js
@@ -1,7 +1,7 @@
 import connection from "../database/database.js";
 
 async function getPosts() {
-    return connection.query('SELECT users.username, users."pictureUrl", posts.comment, posts.url FROM posts JOIN users ON posts."userId"=users.id;');
+    return connection.query('SELECT users.username, users."pictureUrl", posts.comment, posts.url FROM posts JOIN users ON posts."userId"=users.id ORDER BY posts."createdAt" DESC LIMIT 20; ');
 };
 
 async function newPost() {
