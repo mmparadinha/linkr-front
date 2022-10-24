5a53fa8af81b35318bce536f5f47f8b1fd86a9c5
mmparadinha <matheusmparadinha@gmail.com>
2022-10-24 17:01:48 -0300

    Cleaning connection

--- src/database/database.js
+++ src/database/database.js
@@ -5,14 +5,6 @@ dotenv.config();
 
 const { Pool } = pg;
 
-// const connection = new Pool({
-//     host: process.env.HOST,
-//     port: process.env.DATABASE_PORT,
-//     user: process.env.DB_USER,
-//     password: process.env.PASSWORD,
-//     database: process.env.DATABASE_URL,
-// })
-
 const databaseConfig = {
     connectionString: process.env.DATABASE_URL,
     ssl: {
