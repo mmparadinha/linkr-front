d468d00709bb309987e929e3882825fbabdab8d1
Renata Milanez <renatamilanez.dev@gmail.com>
2022-10-27 10:02:15 -0300

    bug fix follow/unfollow

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -5,8 +5,8 @@ import {isAuthenticated} from "../middlewares/linkrMiddlewares.js"
 const userRouter = Router();
 
 userRouter.get('/user/:id', getUserLinkrs);
-userRouter.get('/followers/:followedId', isAuthenticated, isFollowed); //checar se um usuário segue o outro
-userRouter.post('/followers/:followedId', isAuthenticated, startFollowing); //começar a seguir o usuario
+userRouter.get('/followers/:followedId', isAuthenticated, isFollowed);
+userRouter.post('/followers/:followedId', isAuthenticated, startFollowing);
 userRouter.delete('/followers/:followedId', isAuthenticated, stopFollowing);
 
 export default userRouter;
