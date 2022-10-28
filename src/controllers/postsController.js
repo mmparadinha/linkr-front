d266e7db75af4f14e6dfedbf3601c300d114573a
Renata Milanez <renatamilanez.dev@gmail.com>
2022-10-28 12:00:47 -0300

    updated posts controllers

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -2,12 +2,13 @@ import { postRepository } from "../repositories/postsRepository.js";
 import urlMetadata from "url-metadata";
 import { newPostSchema } from "../schemas/validationSchemas.js";
 import connection from "../database/database.js";
+import { STATUS_CODE } from "../enums/statusCode.js";
 
 export async function getPosts(req, res) {
   const { token } = res.locals;
   try {
     const listFollows = (await postRepository.followsAnyone(token)).rows;
-    if (listFollows.length === 0) { return res.sendStatus(204); }
+    if (listFollows.length === 0) { return res.sendStatus(STATUS_CODE.SUCCESSNOCONTENT); }
 
     const listPosts = (await postRepository.getPosts(token)).rows;
     console.log(listPosts)
@@ -28,10 +29,10 @@ export async function getPosts(req, res) {
       })
     );
 
-    res.status(200).send(listPosts);
+    res.status(STATUS_CODE.SUCCESSOK).send(listPosts);
   } catch (error) {
     console.error(error);
-    res.sendStatus(500);
+    res.sendStatus(STATUS_CODE.SERVERERRORINTERNAL);
   }
 }
 
@@ -51,23 +52,16 @@ export async function newPost(req, res) {
 
     const postId = (await postRepository.newPost(userId, url, comment)).rows[0].id;
 
-    res.status(201).send({ postId });
+    res.status(STATUS_CODE.SUCCESSCREATED).send({ postId });
   } catch (error) {
     console.log(error);
-    res.sendStatus(500);
+    res.sendStatus(STATUS_CODE.SERVERERRORINTERNAL);
   }
 }
 
 export async function deletePost(req, res) {
   const { id } = req.params;
-  const token = req.headers.authorization?.replace("Bearer ", "");
-  const verifyToken = await connection.query(
-    `SELECT * FROM sessions WHERE token LIKE $1;`,
-    [token]
-  );
-  if (verifyToken.rows.length === 0 || verifyToken.rows[0].active == false)
-    return res.sendStatus(401);
-  const userId = verifyToken.rows[0].userId;
+  const {userId} = res.locals;
 
   try {
     await connection.query(
@@ -75,34 +69,27 @@ export async function deletePost(req, res) {
       [id, userId]
     );
 
-    res.sendStatus(201);
+    res.sendStatus(STATUS_CODE.SUCCESSCREATED);
   } catch (error) {
     console.log(error);
-    res.status(405).send("não foi possivel deletar o post");
+    res.status(STATUS_CODE.ERRORMETHODNOTALLOWED).send("não foi possivel deletar o post");
   }
 }
 
 export async function updatePost(req, res) {
   const { url, comment } = req.body;
   const { id } = req.params;
-  const token = req.headers.authorization?.replace("Bearer ", "");
-  const verifyToken = await connection.query(
-    `SELECT * FROM sessions WHERE token LIKE $1;`,
-    [token]
-  );
-  if (verifyToken.rows.length === 0 || verifyToken.rows[0].active == false)
-    return res.sendStatus(401);
-  const userId = verifyToken.rows[0].userId;
+  const {userId} = res.locals;
 
   try {
     await connection.query(
       `UPDATE posts SET url = $1, comment = $2 WHERE id = $3 AND "userId" = $4;`,
       [url, comment, id, userId]
     )
-    res.sendStatus(201);
+    res.sendStatus(STATUS_CODE.SUCCESSCREATED);
   } catch (error) {
     console.log(error);
-    res.sendStatus(500);
+    res.sendStatus(STATUS_CODE.SERVERERRORINTERNAL);
   }
 }
 
@@ -113,10 +100,10 @@ export async function countNewPosts(req, res) {
   try {
     const { rows } = (await postRepository.newPostsNumber(token, postId));
 
-    res.status(200).send({ count: rows[0].count });
+    res.status(STATUS_CODE.SUCCESSOK).send({ count: rows[0].count });
   } catch (error) {
     console.log(error);
-    res.sendStatus(500);
+    res.sendStatus(STATUS_CODE.SERVERERRORINTERNAL);
   };
 };
 
